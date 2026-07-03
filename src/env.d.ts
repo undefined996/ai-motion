516064cdd074255aaceeba41bde9d3db251a0b47
Simon <10131203+gaomeng1900@users.noreply.github.com>
2026-07-03 23:45:32 +0800

    chore: legacy code; move in the css version

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,9 @@ declare module '*.glsl' {
 	export default content
 }
 
+declare module '*.module.css' {
+	const classes: Record<string, string>
+	export default classes
+}
+
 declare const __AI_MOTION_VERSION__: string
